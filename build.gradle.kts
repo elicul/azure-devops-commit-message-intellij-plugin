398692c38927fbc2fe1ff9763d04012e2adf87d3
Enzo Licul <enzo.licul@ebe-inc.com>
2026-04-01 23:14:42 +0200

    Build gradle update

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,15 @@ plugins {
 group = properties("pluginGroup")
 version = properties("pluginVersion")
 
+java {
+  sourceCompatibility = JavaVersion.VERSION_11
+  targetCompatibility = JavaVersion.VERSION_11
+}
+
+kotlin {
+  jvmToolchain(11)
+}
+
 repositories {
   mavenCentral()
   maven { url = uri("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies") }
